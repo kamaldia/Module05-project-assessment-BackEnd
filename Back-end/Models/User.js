204fba04af4193f284132ad240074f0d9d5c80a8
Kamal <kamal_dia@hotmail.com>
2024-02-05 11:41:40 +0200

    added user model

---
+++ Back-end/Models/User.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ['admin', 'registered', 'unregistered'],
+    default: 'unregistered',
+  },
+});
+
+export default mongoose.model('User', userSchema);
