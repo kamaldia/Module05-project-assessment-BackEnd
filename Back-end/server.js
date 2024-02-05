d7e863f6af31a6084d48f50dae3dcb2f258789eb
Kamal <kamal_dia@hotmail.com>
2024-02-05 11:31:18 +0200

    connected to db

---
+++ Back-end/server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import mongoose from 'mongoose';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 8000;
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`DB connected and Server is running on ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Error connecting to the database:", err);
+  });
